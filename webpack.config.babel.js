112d712372e877b6c8ef18c07a8516f8022298eb
Cedric Tahina <cedrictahina.rasoamanana@gmail.com>
2017-09-17 14:15:17 +0400

    - Add hot reload to webpack-dev-server

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,17 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin'; 
+import webpack from 'webpack';
 import path from 'path';
+
+console.log('env' + process.env.NODE_ENV);
+const isProd = process.env.NODE_ENV === 'production'; // true or false
+var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: ['css-loader', 'sass-loader'],
+  publicPath: '/build/styles'
+});
+var cssConfig = isProd ? cssProd : cssDev
 
 export default {
   entry: './src/scripts/main.js',
@@ -18,17 +29,16 @@ export default {
       { 
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-          publicPath: '/build/styles'
-        })
+        use: cssProd
       },
       { 
         test: /\.pug$/,
         use: [
           {
-            loader:'html-loader'
+            loader:'html-loader',
+            options: {
+              minimize: false
+            }
           },
           {
             loader: 'pug-html-loader',
@@ -40,13 +50,20 @@ export default {
       }    
     ]
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    open: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/views/index.pug'
     }),
     new ExtractTextPlugin({
-      filename: './styles/main.css'
+      filename: 'styles/main.css',
+      disable: !isProd,
+      allChunks: true
     })
+    // new webpack.NamedModulesPlugin()
   ]
 };
